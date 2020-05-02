dfbd8e4f6dd33362e934d4d94d496ca70f286e0c
Arun Bharhath <arun.bharhath@scripbox.com>
2020-05-02 13:56:43 +0530

    Add search input icon

---
+++ src/js/components/SearchInputIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SearchInputIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+    >
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+        <g fill="#929292">
+          <path d="M19.427 21.427a8.5 8.5 0 112-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 01-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 100-13 6.5 6.5 0 000 13z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default SearchInputIcon;
