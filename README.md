8a862dd18b973f59103540c39e67598bc74bc2bc
Sahil Prajapati <sahilprjpt206@gmail.com>
2020-05-02 12:59:21 +0530

    Add readme to start dev env and prod

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sahiyogi
+
+### Development Setup
+- Make sure that `node` version is set to `12.16.x`.
+- Run the following commands to bring up the application
+```shell
+yarn install
+
+# start the server
+yarn dev
+```
+
+### Production Setup
+Run `yarn build` and `yarn start` to run the app
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,18 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
