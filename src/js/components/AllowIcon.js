f6d00c6f209e2648dd18e20762a750d058b4073c
Arun Bharhath <arun.bharhath@scripbox.com>
2020-05-02 16:23:45 +0530

    Add restrict and allow icons

---
+++ src/js/components/AllowIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AllowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 15 15"
+      version="1.1"
+      viewBox="0 0 15 15"
+      xmlSpace="preserve"
+      fill="#76dab2"
+      width={20}
+      height={20}
+    >
+      <path d="M7.5 0C3.364 0 0 3.364 0 7.5S3.364 15 7.5 15 15 11.636 15 7.5 11.636 0 7.5 0zm0 14C3.916 14 1 11.084 1 7.5S3.916 1 7.5 1 14 3.916 14 7.5 11.084 14 7.5 14z"></path>
+      <path d="M6.505 8.853L3.861 6.209 3.154 6.916 6.51 10.272 11.856 4.851 11.144 4.149z"></path>
+    </svg>
+  );
+}
+
+export default AllowIcon;
---
+++ src/js/components/RestrictIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function RestrictIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 15 15"
+      version="1.1"
+      viewBox="0 0 15 15"
+      xmlSpace="preserve"
+      fill="#da7676"
+      width={20}
+      height={20}
+    >
+      <path d="M7.5 0C3.364 0 0 3.364 0 7.5S3.364 15 7.5 15 15 11.636 15 7.5 11.636 0 7.5 0zm0 14C3.916 14 1 11.084 1 7.5S3.916 1 7.5 1 14 3.916 14 7.5 11.084 14 7.5 14z"></path>
+      <path d="M10.146 4.146L7.5 6.793 4.854 4.146 4.146 4.854 6.793 7.5 4.146 10.146 4.854 10.854 7.5 8.207 10.146 10.854 10.854 10.146 8.207 7.5 10.854 4.854z"></path>
+    </svg>
+  );
+}
+
+export default RestrictIcon;
