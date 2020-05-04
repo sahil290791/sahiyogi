25163862b890e1fc023818233dc0875230a503e1
Sahil Prajapati <sahilprjpt206@gmail.com>
2020-05-04 22:48:31 +0530

    Add config.js.example

--- src/js/Api/index.js
+++ src/js/Api/index.js
@@ -1,9 +1,12 @@
 import { makeGetRequest } from './api';
+import config from '../../../config';
 
-const FOURSQUARE_CLIENT_SECRET = 'HRDKFPHCHB3VXRKAKZEI0UJBDTKTGPJKJ1VKG3VR11MSI2OI';
-const FOURSQUARE_CLIENT_ID = 'G32Z1SB20LPG1UJRLBQV1PKXKTAQV0WIJS50A2F3BD4CKVHN';
-
-const API_URL = process.env.NODE_ENV === 'production' ? '/node-api' : 'http://localhost:5000/node-api';
+const {
+  FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET, COVID_ORG_URL,
+  NODE_API_URL, PINCODE_API_URL
+} = config;
+// const FOURSQUARE_CLIENT_SECRET = 'HRDKFPHCHB3VXRKAKZEI0UJBDTKTGPJKJ1VKG3VR11MSI2OI';
+// const FOURSQUARE_CLIENT_ID = 'G32Z1SB20LPG1UJRLBQV1PKXKTAQV0WIJS50A2F3BD4CKVHN';
 
 const getDataFromLatLang = (lat, lang, options) => {
   makeGetRequest({
@@ -14,7 +17,7 @@ const getDataFromLatLang = (lat, lang, options) => {
 
 const getCityFromPinCode = (pincode, options) => {
   makeGetRequest({
-    url: `https://pincode.saratchandra.in/api/pincode/${pincode}`,
+    url: `${PINCODE_API_URL}/api/pincode/${pincode}`,
     withCredentials: false,
     ...options
   });
@@ -22,7 +25,7 @@ const getCityFromPinCode = (pincode, options) => {
 
 const getLabsForAState = (state, options) => {
   makeGetRequest({
-    url: `http://lockdown-api.scripbox.io/api/v1/labs`,
+    url: `${COVID_ORG_URL}/v1/labs`,
     withCredentials: false,
     showAllowOrigin: false,
     key: 'getLabsForAState',
@@ -32,7 +35,7 @@ const getLabsForAState = (state, options) => {
 
 const getZoneColor = (placeData, options) => {
   makeGetRequest({
-    url: `http://lockdown-api.scripbox.io/api/v1/zone?district_name=${placeData.city}&state_name=${placeData.state}`,
+    url: `${COVID_ORG_URL}/v1/zone?district_name=${placeData.city}&state_name=${placeData.state}`,
     withCredentials: false,
     showAllowOrigin: false,
     key: 'getZoneColor',
@@ -42,7 +45,7 @@ const getZoneColor = (placeData, options) => {
 
 const getActivityData = (zone, options) => {
   makeGetRequest({
-    url: `http://lockdown-api.scripbox.io/api/v1/activities?zone=${zone}`,
+    url: `${COVID_ORG_URL}/v1/activities?zone=${zone}`,
     withCredentials: false,
     showAllowOrigin: false,
     key: 'getActivityData',
@@ -52,7 +55,7 @@ const getActivityData = (zone, options) => {
 
 const getStateHelplineDetails = (state, options) => {
   makeGetRequest({
-    url: `${API_URL}/get_state_wise_helpline_data?state=${state}`,
+    url: `${NODE_API_URL}/get_state_wise_helpline_data?state=${state}`,
     withCredentials: false,
     showAllowOrigin: false,
     key: 'getStateHelplineDetails',
@@ -68,7 +71,3 @@ export {
   getStateHelplineDetails,
   getLabsForAState
 };
-
-// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=711202&inputtype=textquery&fields=formatted_address,id,name,opening_hours,geometry&key=AIzaSyCjt7-Mdz_jiSsXUT4-Teffc9fS3SrmVDA
-// https://maps.googleapis.com/maps/api/place/details/json?place_id=8564fbd0fb148808b9bc82bc788c7c2eb4077fe4&fields=name,rating,formatted_phone_number&key=AIzaSyCjt7-Mdz_jiSsXUT4-Teffc9fS3SrmVDA
-// https://api.foursquare.com/v2/venues/search?ll=22.6322886,88.35064679999999&client_secret=HRDKFPHCHB3VXRKAKZEI0UJBDTKTGPJKJ1VKG3VR11MSI2OI&client_id=G32Z1SB20LPG1UJRLBQV1PKXKTAQV0WIJS50A2F3BD4CKVHN&v=20160623
