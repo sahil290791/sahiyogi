13d3e47dd430a29c6ea5d52aaacfcd07506c403c
Arun Bharhath <arun.bharhath@scripbox.com>
2020-05-04 20:16:34 +0530

    Add powered by logo

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScripboxLogo from './ScripboxLogo';
 
 const Header = (props) => {
   const { title, description } = props;
   return (
     <header className="c19-header">
       <h1>{title}</h1>
+      <div className='d-flex flex-direction-row justify-content-center align-items-center mb-3'>
+        <div className='d-flex powered-by-text mr-1'>Powered by</div>
+        <div className='d-flex'><ScripboxLogo /></div>
+      </div>
       <p className='text-center c19-info-text'>{description}</p>
     </header>
   );
