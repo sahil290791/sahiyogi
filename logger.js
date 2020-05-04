8f0c4ff09368816d9a81c85ebf4f5d1536e14494
Sahil Prajapati <sahilprjpt206@gmail.com>
2020-05-04 23:18:01 +0530

    Fix logging

--- logger.js
+++ logger.js
@@ -1,22 +1,14 @@
 import winston from 'winston';
-import _ from 'lodash-deep';
 
-const logger = new (winston.Logger)({
+const logger = winston.createLogger({
+  levels: winston.config.syslog.levels,
   transports: [
-    new (winston.transports.File)({ filename: 'log/server.log' }),
-    new (winston.transports.Console)
+    new winston.transports.Console({ level: 'error' }),
+    new winston.transports.File({
+      filename: 'logs/server.log',
+      level: 'info'
+    })
   ]
-});
-
-logger.filters.push((level, msg, meta) => {
-  const filteredKeys = ['access_token', 'password'];
-  const regex = new RegExp(filteredKeys.join('|'));
-  msg = msg.replace(regex, '******');
-  meta = _.deepMapValues(meta, (value, path) => {
-    return regex.test(path) ? 'FILTERED' : value;
-  });
-
-  return { msg, meta };
 });
 
 module.exports = logger;
