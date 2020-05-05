e4d31648eb8fed3893a0724db1327767e6506f5d
Arun Bharhath <arun.bharhath@scripbox.com>
2020-05-05 19:41:21 +0530

    Update sb logo

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -7,9 +7,9 @@ const Header = (props) => {
   return (
     <header className="c19-header">
       <h1>{title}</h1>
-      <div className='d-flex flex-direction-row justify-content-center align-items-center mb-3'>
-        <div className='d-flex powered-by-text mr-1'>Powered by</div>
-        <div className='d-flex'><ScripboxLogo /></div>
+      <div className='d-flex justify-content-center align-items-center mb-3'>
+        <div className='d-flex powered-by-text mr-1'>Co-created by</div>
+        <div className='d-flex sb-logo'><ScripboxLogo /></div>
       </div>
       <p className='text-center c19-info-text'>{description}</p>
     </header>
