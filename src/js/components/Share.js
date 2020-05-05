d4254a85c0a88b10f1b6790ea106bb9c1c8e7f8e
Arun Bharhath <arun.bharhath@scripbox.com>
2020-05-05 19:56:01 +0530

    Update share message

--- src/js/components/Share.js
+++ src/js/components/Share.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const Share = () => {
-  const whatsappMessage =
-    'Find out what\'s allowed and restricted in your area during the COVID-19 lockdown.\nhttps://covidlockdown.org/';
+  const shareMessage = 'Find out what\'s allowed and restricted in your area during the COVID-19 lockdown.\nhttps://covidlockdown.org/';
   return (
     <div className='c19-share-section text-center mt-3'>
       <a className="c19-share-link facebook" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcovidlockdown.org" target="_blank" rel="noreferrer noopener">
         <span className='d-inline-block'><i className='fab fa-facebook-square fa-2x' /></span>
       </a>
-      <a className="c19-share-link twitter" href="https://twitter.com/intent/tweet/?text=Find%20out%20what's%20allowed%20and%20restricted%20in%20your%20area%20during%20the%20COVID-19%20lockdown.&url=https%3A%2F%2Fcovidlockdown.org" target="_blank" rel="noreferrer noopener">
+      <a
+        className="c19-share-link twitter"
+        href={`https://twitter.com/intent/tweet/?text=${encodeURIComponent(shareMessage)}`}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         <span className='d-inline-block mx-3'><i className='fab fa-twitter-square fa-2x' /></span>
       </a>
       <a
         className="c19-share-link whatsapp"
-        href={`https://api.whatsapp.com/send?text=${encodeURIComponent(whatsappMessage)}`}
+        href={`https://api.whatsapp.com/send?text=${encodeURIComponent(shareMessage)}`}
         target="_blank"
         rel="noreferrer noopener"
       >
